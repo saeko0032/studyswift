800b82eea799b8edcf0ec33d660a312bea1b7c4a
Saeko Fukui <saeko0032@gmail.com>
2017-04-20 12:50:19 -0700

    Create listing8-9.swift

---
+++ writing code/listing8-9.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var errorCodeString: String?
+errorCodeString = "404"
+var errorDescription: String?
+
+if let theError = errorCodeString, let errorCodeInteger = Int(theError), errorCodeInteger == 404 {
+    errorDescription = "\(errorCodeInteger + 200): resource was not found."
+}
+
+var upCaseErrorDescription = errorDescription?.uppercased()
+errorDescription
