d866028660ad4fcb21a7a4ad8e7ded19e7ea1291
Saeko Fukui <saeko0032@gmail.com>
2017-11-25 00:44:05 -0800

    Create NestingLoops.swift

---
+++ NestingLoops.swift
@@ -0,0 +1,12 @@
+while /*#-editable-code Condition*/!isBlocked/*#-end-editable-code*/ {
+    while /*#-editable-code Condition*/!isOnGem/*#-end-editable-code*/ {
+        //#-editable-code Add commands to your loop
+        moveForward()
+        //#-end-editable-code
+    }
+    //#-editable-code Add commands to your loop
+    turnLeft()
+    collectGem()
+
+    //#-end-editable-code
+}
